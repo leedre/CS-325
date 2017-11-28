bc509cc05b810a9f8af90af6ec0601f1e55edc8b
Jon-Eric Cook <mrjec@protonmail.com>
2017-11-27 16:08:36 -0800

    turned in

--- Project-TSP/turn_in/run-tsp-verifier.sh
+++ Project-TSP/turn_in/run-tsp-verifier.sh
@@ -5,7 +5,7 @@ do
 	file_name=tsp_example_${NUM}.txt
 	python ./tsp-verifier.py $file_name ${file_name}.tour
 done
-for NUM in {2..8}
+for NUM in {1..7}
 do
 	file_name=test-input-${NUM}.txt
 	python ./tsp-verifier.py $file_name ${file_name}.tour
